1f49fa82afd95f7ed836634b5efaf17f610d0672
jbjzeehad <ilamta.97@gmail.com>
2022-08-03 20:34:24 +0600

    part 01

---
+++ working_with_strings.py
@@ -0,0 +1,31 @@
+# @ Jbj Zeehad
+
+x = "Hello world" #string
+print(x)
+
+print("One \nTwo \nThree") #special characters
+print ("One\tTwo\tThree")
+print('Brian\'s mother : He\'s not an angel.He\'s a very naughty boy!') #escape character
+
+y="Hello"      #Accessing Strings
+print(y[2])
+print(y[-1])
+for c in y:
+    print(c)    #loop
+
+z="I love Pyton"  #string oprations
+if "love" in z:
+    print("Yes")
+
+print("spam" + ' egggs')   #concatenation
+print(4 * '2')
+print("spam " * 3)
+
+line="This is some text"   #string functions
+print(line.count("s"))
+print (line.upper())
+print(line.lower())
+print(line.replace("some text","awesome"))
+print(len(line))
+
+#part 01
