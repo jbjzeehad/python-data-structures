b65fd33f5227d715ff139357ced1095f7089c702
jbjzeehad <ilamta.97@gmail.com>
2022-08-03 21:49:07 +0600

    part 02

--- lists.py
+++ lists.py
@@ -9,7 +9,28 @@ m= [
 ]
 print(m[1][2])  #out: 06
 
+words=["spam","eggs","sausage","spam"]      #list operations
+print("spam" in words)      #True
+print("egg" in words)       #False
+print("tomato" in words)    #False
 
+x=[2,4,6,8]  #loops
+for n in x:
+    print(n) #out : 2,4,6,8
 
+y=[2,4,6]   #list functions
+y.append(8)
+y.remove(4)
+y.insert(0,8)
+print(y)            #o:[8,2,6,8]
+print(y.count(8))   #o: 2
+
+z=[2,4,6,8]         
+z.reverse()
+print(z)        #o:[8,6,4,2]
+z.sort()
+print(z)        #o:[2,4,6,8]
+print(min(z))   #o:2
+print(max(z))   #o:8
 
 #part 02
