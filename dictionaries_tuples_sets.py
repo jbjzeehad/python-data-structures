9d11945ba765a0f1ad0008bc59fecadd06edd446
jbjzeehad <ilamta.97@gmail.com>
2022-08-04 15:58:53 +0600

    part 03

--- dictionaries_tuples_sets.py
+++ dictionaries_tuples_sets.py
@@ -1,3 +1,14 @@
 # @ Jbj Zeehad
 
+
+ages={
+    "Dave" : 24,
+    "Mary" : 42,
+    "John" : 58
+}                       #dictionaries > key : value
+
+print(ages["Dave"])     
+print(ages["Mary"])
+
+
 #part 03
