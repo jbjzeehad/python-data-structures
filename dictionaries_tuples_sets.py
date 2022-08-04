c9cc665de5d9fb7f820b1cc045639f5c5eec0b06
jbjzeehad <ilamta.97@gmail.com>
2022-08-04 18:07:20 +0600

    part 03

--- dictionaries_tuples_sets.py
+++ dictionaries_tuples_sets.py
@@ -9,6 +9,39 @@ ages={
 
 print(ages["Dave"])     
 print(ages["Mary"])
+                    
+nums={
+    1:"one",
+    2:"two",
+    3:"three"
+}                       #dictionary function
+print(1 in nums)
+print("three" in nums)      #False
+print(4 not in nums)        #True
+
+
+pairs={
+    1:"apple",
+    "orange" : [2,3,4],
+    True : False,
+    12 : "True"
+}                   #get() function
+
+print(pairs.get("orange"))          #o: [2,3,4]
+print(pairs.get(7,42))              #o: 42
+print(pairs.get(12345,"not found")) #o: not found
+
+#Tuples
+words=("spam","eggs","sausages")
+print(words[0])                 #o: spam
+
+dict={
+    ("David",42) : "red",
+    ("Bob",24) : "green"
+}
+print(dict[("Bob",24)])    #o: green
+
+
 
 
 #part 03
